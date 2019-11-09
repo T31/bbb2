d52ff71e5b605b9e673cebd2a8f2d963ea007df7
Michael Kim <michaelkim1219@gmail.com>
2019-11-09 15:43:12 -0800

    Added BackblazeB2Error.py

---
+++ BackblazeB2Error.py
@@ -0,0 +1,11 @@
+class BackblazeB2Error(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class BackblazeB2ConnectError(BackblazeB2Error):
+    def __init__(self, message):
+        super().__init__(message)
+
+class BackblazeB2ExpiredAuthError(BackblazeB2Error):
+    def __init__(self, message):
+        super().__init__(message)
