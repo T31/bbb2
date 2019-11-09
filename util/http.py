bcb17fc66d00802c1627533f3487a54514d4eb59
Michael Kim <michaelkim1219@gmail.com>
2019-11-08 22:47:12 -0800

    Added Url class to http.py

--- util/http.py
+++ util/http.py
@@ -1,7 +1,13 @@
+import enum
+
+class Protocol(enum.Enum):
+    HTTP = 0
+    HTTPS = 1
+
 class Domain:
     domain = []
 
-    def __init__(self, domain):
+    def __init__(self, domain = []):
         self.domain = domain
 
     def __str__(self):
@@ -16,11 +22,11 @@ class Domain:
 class Path:
     path = []
 
-    def __init__(self, path):
+    def __init__(self, path = []):
         self.path = path
 
     def __str__(self):
-        retVal = ""
+        retVal = "/"
         separator = ""
         for word in self.path:
             retVal += separator + word
@@ -28,3 +34,24 @@ class Path:
 
         return retVal
 
+class Url:
+    protocol = Protocol.HTTPS
+    domain = Domain()
+    path = Path()
+
+    def __init__(self, protocol, domain, path):
+        self.protocol = protocol
+        self.domain = domain
+        self.path = path
+
+    def __str__(self):
+        protoString = ""
+        if Protocol.HTTP == self.protocol:
+            protoString = "http"
+        elif Protocol.HTTPS == self.protocol:
+            protoString = "https"
+        else:
+            raise BackblazeB2Error("Invalid protocol enum ("
+                                   + str(self.protocol) + ")")
+
+        return protoString + "://" + str(self.domain) + str(self.path)
