a73104baacb2067ada2375c17bd4e3cce7cd1658
Michael Kim <michaelkim1219@gmail.com>
2019-11-09 22:50:45 -0800

    Added expired auth detection

--- util/api.py
+++ util/api.py
@@ -5,14 +5,21 @@ from BackblazeB2Error import BackblazeB2Error
 import util.http
 
 def send_request(url, method, headers, body):
-    response = util.http.send_request(url, method, headers, body)
-
-    if http.HTTPStatus.OK != response.status_code:
-        msg = "HTTP response status wasn't OK(200). " + str(response)
-        raise BackblazeB2Error(msg)
-
     try:
-        return json.loads(str(object=response.resp_body, encoding='utf-8'))
+        response = util.http.send_request(url, method, headers, body)
+        json_body = json.loads(str(object=response.resp_body, encoding='utf-8'))
+
+        if http.HTTPStatus.UNAUTHORIZED == response.status_code:
+            if "expired_auth_token" == json_body["code"]:
+                raise BackblazeB2ExpiredAuthError(str(response))
+        if http.HTTPStatus.OK != response.status_code:
+            msg = "HTTP response status wasn't OK(200). " + str(response)
+            raise BackblazeB2Error(msg)
+
+        return json_body
     except JSONDecodeError as e:
         msg = "Malformed JSON response. " + str(response)
+        raise BackblazeB2Error(msg) from e
+    except KeyError as e:
+        msg = "Missing key from response. " + str(response)
         raise BackblazeB2Error(msg) from e
