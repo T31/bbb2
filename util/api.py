237e2ebfae7c135302d68d4b0af229a567b080c5
Michael Kim <michaelkim1219@gmail.com>
2019-12-16 22:46:32 -0800

    Added support for PARTIAL_CONTENT HTTP response

--- util/api.py
+++ util/api.py
@@ -4,7 +4,7 @@ import json
 from BackblazeB2Error import BackblazeB2Error
 import util.http
 
-def send_request(url, method, headers, body):
+def send_request(url, method, headers, body, partial=False):
     try:
         response = util.http.send_request(url, method, headers, body)
         json_body = json.loads(str(object=response.resp_body, encoding='utf-8'))
@@ -12,9 +12,19 @@ def send_request(url, method, headers, body):
         if http.HTTPStatus.UNAUTHORIZED == response.status_code:
             if "expired_auth_token" == json_body["code"]:
                 raise BackblazeB2ExpiredAuthError(str(response))
-        if http.HTTPStatus.OK != response.status_code:
-            msg = "HTTP response status wasn't OK(200). " + str(response)
-            raise BackblazeB2Error(msg)
+
+        if partial:
+            if ((http.HTTPStatus.OK != response.status_code)
+                and (http.HTTPStatus.PARTIAL_CONTENT != response.status_code)):
+
+                msg = "Bad HTTP response status code "
+                msg += str(reponse.status_code) + "."
+                raise BackblazeB2Error(msg)
+        else:
+            if http.HTTPStatus.OK != response.status_code:
+                msg = "Bad HTTP response status code "
+                msg += str(reponse.status_code) + "."
+                raise BackblazeB2Error(msg)
 
         return json_body
     except json.JSONDecodeError as e:
