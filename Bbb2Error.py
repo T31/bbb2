94b58134b2f5c65c803a81ce9590e2c3544b979f
Michael Kim <michael-kim@nowhere>
2020-08-13 19:03:52 -0700

    Changed error names to Bbb2Error

--- Bbb2Error.py
+++ Bbb2Error.py
@@ -1,31 +1,31 @@
-class BackblazeB2Error(Exception):
+class Bbb2Error(Exception):
     def __init__(self, message):
         super().__init__(message)
 
-class BackblazeB2ApiParseError(BackblazeB2Error):
+class ApiParseError(Bbb2Error):
     def __init__(self, message):
         super().__init__(message)
 
-class BackblazeB2BadRequestError(BackblazeB2Error):
+class BadRequestError(Bbb2Error):
     def __init__(self, message):
         super().__init__(message)
 
-class BackblazeB2ConnectError(BackblazeB2Error):
+class ConnectError(Bbb2Error):
     def __init__(self, message):
         super().__init__(message)
 
-class BackblazeB2ExpiredAuthError(BackblazeB2Error):
+class ExpiredAuthError(Bbb2Error):
     def __init__(self, message):
         super().__init__(message)
 
-class BackblazeB2InternalError(BackblazeB2Error):
+class InternalError(Bbb2Error):
     def __init__(self, message):
         super().__init__(message)
 
-class BackblazeB2RemoteError(BackblazeB2Error):
+class RemoteError(Bbb2Error):
     def __init__(self, message):
         super().__init__(message)
 
-class BackblazeB2UnauthorizedError(BackblazeB2Error):
+class UnauthorizedError(Bbb2Error):
     def __init__(self, message):
         super().__init__(message)
