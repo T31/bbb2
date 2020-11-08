4eb64dba79a93413ef558558d6dbd47f7d0fa58a
Michael Kim <null>
2020-11-07 23:22:28 -0800

    Removed api.util.send_request

--- api/util.py
+++ api/util.py
@@ -2,33 +2,6 @@ import http
 import json
 
 from Bbb2Error import BadRequestError
-import util.http
-
-def send_request(url, method, headers, body,
-                 good_status_codes = [http.HTTPStatus.OK]):
-    try:
-        response = util.http.send_request(url, method, headers, body)
-
-        if response.status_code in good_status_codes:
-            return response
-        elif http.HTTPStatus.UNAUTHORIZED == response.status_code:
-            resp_body = json.loads(str(object=response.resp_body,
-                                       encoding='utf-8'))
-            if "expired_auth_token" == resp_body["code"]:
-                raise Bbb2Error.ExpiredAuthError(str(response))
-            else:
-                raise Bbb2Error.UnauthorizedError(str(response))
-        elif http.HTTPStatus.BAD_REQUEST == response.status_code:
-            raise Bbb2Error.BadRequestError(str(response))
-        elif http.HTTPStatus.INTERNAL_SERVER_ERROR == response.status_code:
-            raise Bbb2Error.RemoteError(str(response))
-        else:
-            msg = "Unhandled HTTP response status code."
-            msg += " StatusCode=" + str(response.status_code)
-            msg += ", Response=\"" + str(response) + "\"."
-            raise Bbb2Error.ApiParseError(msg)
-    except (json.JSONDecodeError, KeyError) as e:
-        raise Bbb2Error.ApiParseError(str(response)) from e
 
 def raise_appropriate_error(http_response):
     bb_code = None
