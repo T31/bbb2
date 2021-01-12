2b5e83bc569fad2cdec5997377e4b30130c2adfe
Michael Kim <null>
2021-01-11 23:23:00 -0800

    Added unit test framework (JUnit)

---
+++ java/tst/Tests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Tests
+{
+    @Test
+    void fucko()
+    {
+        System.out.println("WTF");
+        Assertions.assertEquals(true, true);
+    }
+}
