a6fa5349a727f7417b3dc2d11e9d7238b38fbd69
Michael Kim <null>
2021-01-11 22:35:12 -0800

    Added authorize call to RawApi class
    Quick test shows connection is working.

--- java/src/Bbb2.java
+++ java/src/Bbb2.java
@@ -1,11 +1,14 @@
-import java.net.http.HttpClient;
+package Bbb2;
 
-import com.google.gson.Gson;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import Bbb2.Api.RawApi;
 
 class Bbb2
 {
     public static void main(String[] args)
     {
-        return;
+        RawApi.authorizeAccount("00259d5d420cefb0000000005", "K00223t/RxR6JJ01YlnFvUqud6/v4c4");
     }
 }
