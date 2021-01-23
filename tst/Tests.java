c267d685b1fe2d92b4334afb74ca113865c76d80
Michael Kim <null>
2021-01-23 00:42:18 -0800

    Completed unit test for AuthorizeAccountResults

--- tst/Tests.java
+++ tst/Tests.java
@@ -10,33 +10,36 @@ class Tests
     {
         String test =
         "{" +
-            "\"absoluteMinimumPartSize\": 5000000," +
-            "\"accountId\": \"YOUR_ACCOUNT_ID\"," +
-            "\"allowed\":" +
-            "{" +
-                "\"bucketId\": \"BUCKET_ID\"," +
-                "\"bucketName\": \"BUCKET_NAME\"," +
-                "\"capabilities\":" +
-                "[" +
-                    "\"listBuckets\"," +
-                    "\"listFiles\"," +
-                    "\"readFiles\"," +
-                    "\"shareFiles\"," +
-                    "\"writeFiles\"," +
-                    "\"deleteFiles\"" +
-                "]," +
-                "\"namePrefix\": null," +
-                "\"apiUrl\": \"https://apiNNN.backblazeb2.com\"," +
-                "\"authorizationToken\": \"4_0022623512fc8f80000000001_0186e431_d18d02_acct_tH7VW03boebOXayIc43-sxptpfA=\"," +
-                "\"downloadUrl\": \"https://f002.backblazeb2.com\"," +
-                "\"recommendedPartSize\": 100000000" +
-            "}" +
+        "  \"absoluteMinimumPartSize\": 5000000," +
+        "  \"accountId\": \"YOUR_ACCOUNT_ID\"," +
+        "  \"allowed\": {" +
+        "    \"bucketId\": \"BUCKET_ID\"," +
+        "    \"bucketName\": \"BUCKET_NAME\"," +
+        "    \"capabilities\": [" +
+        "      \"listBuckets\"," +
+        "      \"listFiles\"," +
+        "      \"readFiles\"," +
+        "      \"shareFiles\"," +
+        "      \"writeFiles\"," +
+        "      \"deleteFiles\"" +
+        "    ]," +
+        "    \"namePrefix\": null" +
+        "  }," +
+        "  \"apiUrl\": \"https://apiNNN.backblazeb2.com\"," +
+        "  \"authorizationToken\": \"4_0022623512fc8f80000000001_0186e431_d18d02_acct_tH7VW03boebOXayIc43-sxptpfA=\"," +
+        "  \"downloadUrl\": \"https://f002.backblazeb2.com\"," +
+        "  \"recommendedPartSize\": 100000000" +
         "}";
 
         AuthorizeAccountResult result = new AuthorizeAccountResult(test);
-        AuthorizeAccountResult.Internals i = new AuthorizeAccountResult.Internals();
-        System.out.println(result.apiUrl);
-        System.out.println(result.accountId);
-        Assertions.assertEquals(true, true);
+        Assertions.assertEquals(result.accountId, "YOUR_ACCOUNT_ID");
+        Assertions.assertEquals(result.authToken,
+                                "4_0022623512fc8f80000000001_0186e431_d18d02_acct_tH7VW03boebOXayIc43-sxptpfA=");
+        Assertions.assertEquals(result.apiUrl.toString(),
+                                "https://apiNNN.backblazeb2.com");
+        Assertions.assertEquals(result.downloadUrl.toString(),
+                                "https://f002.backblazeb2.com");
+        Assertions.assertEquals(result.minPartSizeBytes, 5000000);
+        Assertions.assertEquals(result.recPartSizeBytes, 100000000);
     }
 }
